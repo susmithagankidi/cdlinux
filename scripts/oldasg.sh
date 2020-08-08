6e061bca6fd65ec63445974aa8e046ac04ef2878
susmithagankidi <gankidisusmitha@gmail.com>
2020-08-08 16:16:47 -0400

    Create oldasg.sh

---
+++ scripts/oldasg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id 2>/dev/null`
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document 2>/dev/null | jq -r .region`
+ASG=`aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --region $REGION --output json | jq -r '.[][] | select(.Key=="aws:autoscaling:groupName") | .Value'`
+echo "$ASG"
+aws cloudwatch delete-alarm --alarm-name "$ASG-alarm" --region $REGION
