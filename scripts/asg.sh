40282180dc4233dce997536dd31871ccfa5c0d1b
susmithagankidi <gankidisusmitha@gmail.com>
2020-08-08 11:17:25 -0400

    Update asg.sh

--- scripts/asg.sh
+++ scripts/asg.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id 2>/dev/null`
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document 2>/dev/null | jq -r .region`
+ASG=`aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --region $REGION --output json | jq -r '.[][] | select(.Key=="aws:autoscaling:groupName") | .Value'`
+echo "$ASG"
